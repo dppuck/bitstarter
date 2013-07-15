0718b3060d3f5ace780441bd69011f3ff1198dd4
D Pascal <dwaynepascal@yahoo.co.uk>
2013-07-15 13:48:58 +0000

    Updated web.js with index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+	var inFile = "index.html";
+
+	var fileData = fs.readFileSync(inFile);
+	response.send(fileData.toString());
 });
 
 var port = process.env.PORT || 5000;
