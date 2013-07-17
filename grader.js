286afc058e3ce8ee83d4cf68ac3541f7a7e2d1aa
D Pascal <dwaynepascal@yahoo.co.uk>
2013-07-17 19:43:15 +0000

    Final grader.js and checks.json for HW3.

--- grader.js
+++ grader.js
@@ -23,9 +23,13 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var sys     = require('util');
 var cheerio = require('cheerio');
+var rest    = require('./restler');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT  = "http://whispering-fjord-5338.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
+var tmpHtmlFileName     = "temp_index.html";	
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,11 +68,33 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if ( program.file ) {
+
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+
+    } else if ( program.url ) {
+
+	rest.get(program.url).on('complete', function(data) {
+
+		fs.writeFileSync( tmpHtmlFileName, data );
+		var checkJson = checkHtmlFile(tmpHtmlFileName, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	});
+
+    } else {
+
+	console.log("Error: You must specify either a file (-f) or URL (-u) to check.\n");	
+
+    }
+
+   
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
